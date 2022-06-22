c4f44e5763a39663745931dda9d8b6b5dc0fb7fa
ononoki1 <me@ononoki.org>
2022-06-22 13:46:19 +0800

    Use static build

--- build.sh
+++ build.sh
@@ -57,11 +57,10 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --without-http_upstream_random_module --without-http_upstream_zone_module \
 --without-http_userid_module --without-http_uwsgi_module \
 --with-zlib=modules/zlib \
---with-cc-opt="-Imodules/openssl/build/include -fstack-protector-strong -Wno-sign-compare" \
+--with-cc-opt="-static -Imodules/openssl/build/include -fstack-protector-strong -Wno-sign-compare" \
 --with-ld-opt="-ljemalloc -Lmodules/openssl/build/lib64" > /dev/null 2>&1
 make -j$(nproc) > /dev/null 2>&1
 mv objs/nginx ..
-ls -A objs
 cd ..
 hash=$(sha256sum nginx | awk '{print $1}')
 patch=$(cat /github/workspace/patch)
