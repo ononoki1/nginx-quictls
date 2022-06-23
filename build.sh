6aac1b9284d46566b2d84498d840a8dddef3572c
ononoki <me@ononoki.org>
2022-06-23 14:19:30 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo Install dependencies.
 apt-get update > /dev/null 2>&1
 apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential \
 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
-cmake curl git libjemalloc-dev libmaxminddb-dev libpcre2-dev mercurial > /dev/null 2>&1
+cmake curl git libmaxminddb-dev libpcre2-dev mercurial > /dev/null 2>&1
 echo Fetch nginx-quic source code.
 hg clone -b quic https://hg.nginx.org/nginx-quic > /dev/null 2>&1
 echo Fetch quictls source code.
@@ -43,8 +43,8 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --without-http_upstream_zone_module --without-http_userid_module \
 --without-http_uwsgi_module --with-zlib=modules/zlib --with-openssl=modules/openssl \
 --with-openssl-opt="-Wl,-ljemalloc -fpie -Wl,-pie -fpic -O3 -pipe -Wl,-z,now -Wl,-z,relro enable-ktls no-comp enable-ec_nistp_64_gcc_128 no-ssl3-method no-tls1-method no-tls1_1-method no-dtls1-method no-dtls1_2-method" \
---with-cc-opt="-Wl,-ljemalloc -fpie -Wl,-pie -fpic -O3 -pipe -Wl,-z,now -Wl,-z,relro"
-make -j$(nproc)
+--with-cc-opt="-Wl,-ljemalloc -fpie -Wl,-pie -fpic -O3 -pipe -Wl,-z,now -Wl,-z,relro" > /dev/null 2>&1
+make -j$(nproc) > /dev/null 2>&1
 mv objs/nginx ..
 cd ..
 hash=$(ls -l nginx | awk '{print $5}')
