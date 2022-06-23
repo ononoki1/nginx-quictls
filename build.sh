f34c47b7eb1e819777d0949eb3fecec47784097a
ononoki <me@ononoki.org>
2022-06-23 12:31:46 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --without-http_upstream_zone_module --without-http_userid_module \
 --without-http_uwsgi_module --with-zlib=modules/zlib --with-openssl=modules/openssl \
 --with-openssl-opt="-ljemalloc -fpie -Wl,-pie -fpic -shared -O3 -pipe -Wl,-z,defs -Wl,-z,now -Wl,-z,relro -flto enable-ktls no-comp enable-ec_nistp_64_gcc_128 no-ssl3-method no-tls1-method no-tls1_1-method no-dtls1-method no-dtls1_2-method" \
---with-cc-opt="-ljemalloc -fpie -Wl,-pie -fpic -shared -O3 -pipe -Wl,-z,defs -Wl,-z,now -Wl,-z,relro -flto"
+--with-cc-opt="-ljemalloc -fpie -Wl,-pie -fpic -O3 -pipe -Wl,-z,defs -Wl,-z,now -Wl,-z,relro -flto"
 make -j$(nproc)
 mv objs/nginx ..
 cd ..
