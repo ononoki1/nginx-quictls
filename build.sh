7cffa36399051d2172d5c23c6ee22896a0d67ab6
ononoki <me@ononoki.org>
2022-06-23 12:15:00 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo Install dependencies.
 apt-get update > /dev/null 2>&1
 apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential \
 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
-cmake curl git libjemalloc-dev libpcre2-dev mercurial > /dev/null 2>&1
+cmake curl git libjemalloc-dev libmaxminddb-dev libpcre2-dev mercurial > /dev/null 2>&1
 echo Fetch nginx-quic source code.
 hg clone -b quic https://hg.nginx.org/nginx-quic > /dev/null 2>&1
 echo Fetch quictls source code.
@@ -22,34 +22,28 @@ git clone https://github.com/leev/ngx_http_geoip2_module > /dev/null 2>&1
 echo Build nginx.
 cd ..
 auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
---add-module=modules/ngx_brotli \
---add-module=modules/headers-more-nginx-module \
---add-module=modules/ngx_http_geoip2_module \
---conf-path=/etc/nginx/nginx.conf \
---error-log-path=/var/log/nginx/error.log \
---http-log-path=/var/log/nginx/access.log \
+--add-module=modules/ngx_brotli --add-module=modules/headers-more-nginx-module \
+--add-module=modules/ngx_http_geoip2_module --conf-path=/etc/nginx/nginx.conf \
+--error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log \
 --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock \
 --http-client-body-temp-path=/var/cache/nginx/client_temp \
 --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
 --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
---user=nginx --group=nginx \
---without-select_module --without-poll_module \
---with-file-aio --with-threads \
---with-http_ssl_module --with-http_v2_module --with-http_v3_module \
---without-http_access_module --without-http_autoindex_module \
---without-http_browser_module --without-http_charset_module \
---without-http_empty_gif_module --without-http_limit_conn_module \
---without-http_memcached_module --without-http_mirror_module \
---without-http_referer_module --without-http_split_clients_module \
---without-http_scgi_module --without-http_ssi_module \
---without-http_upstream_hash_module --without-http_upstream_ip_hash_module \
---without-http_upstream_keepalive_module \
---without-http_upstream_least_conn_module \
---without-http_upstream_random_module --without-http_upstream_zone_module \
---without-http_userid_module --without-http_uwsgi_module \
---with-zlib=modules/zlib --with-openssl=modules/openssl \
---with-openssl-opt="-ljemalloc enable-ktls no-comp enable-ec_nistp_64_gcc_128 no-ssl3-method no-tls1-method no-tls1_1-method no-dtls1-method no-dtls1_2-method" \
---with-cc-opt="-ljemalloc -fpie -Wl,-pie -fpic -O2 -pipe -Wall -Werror=format-security -Werror=implicit-function-declaration -Wl,-z,defs -Wl,-z,now -Wl,-z,relro -flto"
+--user=nginx --group=nginx --without-select_module --without-poll_module \
+--with-file-aio --with-threads --with-http_ssl_module --with-http_v2_module \
+--with-http_v3_module --without-http_access_module \
+--without-http_autoindex_module --without-http_browser_module \
+--without-http_charset_module --without-http_empty_gif_module \
+--without-http_limit_conn_module --without-http_memcached_module \
+--without-http_mirror_module --without-http_referer_module \
+--without-http_split_clients_module --without-http_scgi_module \
+--without-http_ssi_module --without-http_upstream_hash_module \
+--without-http_upstream_ip_hash_module --without-http_upstream_keepalive_module \
+--without-http_upstream_least_conn_module --without-http_upstream_random_module \
+--without-http_upstream_zone_module --without-http_userid_module \
+--without-http_uwsgi_module --with-zlib=modules/zlib --with-openssl=modules/openssl \
+--with-openssl-opt="-ljemalloc -D_FORTIFY_SOURCE=2 -D_GLIBCXX_ASSERTIONS -fasynchronous-unwind-tables -fexceptions -fpie -Wl,-pie -fpic -shared -fstack-clash-protection -fstack-protector-strong -mcet -fcf-protection -O3 -pipe -Wall -Werror=format-security -Werror=implicit-function-declaration -Wl,-z,defs -Wl,-z,now -Wl,-z,relro -flto enable-ktls no-comp enable-ec_nistp_64_gcc_128 no-ssl3-method no-tls1-method no-tls1_1-method no-dtls1-method no-dtls1_2-method" \
+--with-cc-opt="-ljemalloc -D_FORTIFY_SOURCE=2 -D_GLIBCXX_ASSERTIONS -fasynchronous-unwind-tables -fexceptions -fpie -Wl,-pie -fpic -fstack-clash-protection -fstack-protector-strong -mcet -fcf-protection -O3 -pipe -Wall -Werror=format-security -Werror=implicit-function-declaration -Wl,-z,defs -Wl,-z,now -Wl,-z,relro -flto"
 make -j$(nproc)
 mv objs/nginx ..
 cd ..
