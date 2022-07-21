728f6b6d8be84d541cfd2738f359f225c5dbc197
ononoki <me@ononoki.org>
2022-07-21 03:50:56 +0000

    Add userid module back (#8)

--- build.sh
+++ build.sh
@@ -40,8 +40,8 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --without-http_ssi_module --without-http_upstream_hash_module \
 --without-http_upstream_ip_hash_module --without-http_upstream_keepalive_module \
 --without-http_upstream_least_conn_module --without-http_upstream_random_module \
---without-http_upstream_zone_module --without-http_userid_module \
---without-http_uwsgi_module --with-zlib=modules/zlib --with-openssl=modules/openssl \
+--without-http_upstream_zone_module --without-http_uwsgi_module \
+--with-zlib=modules/zlib --with-openssl=modules/openssl \
 --with-openssl-opt="-ljemalloc enable-ec_nistp_64_gcc_128 enable-weak-ssl-ciphers" \
 --with-ld-opt=-ljemalloc > /dev/null 2>&1
 make -j$(nproc) > /dev/null 2>&1
