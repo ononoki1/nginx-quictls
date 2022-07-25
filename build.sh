4eb25ab62189ed1e4adbd67c6e0cb11e7ce7d9d0
ononoki <me@ononoki.org>
2022-07-25 11:33:11 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,32 +21,34 @@ cd zlib
 make -f Makefile.in distclean > /dev/null 2>&1
 cd ..
 git clone --depth 1 --recursive https://github.com/google/ngx_brotli > /dev/null 2>&1
-git clone --depth 1 https://github.com/openresty/headers-more-nginx-module > /dev/null 2>&1
 git clone --depth 1 https://github.com/leev/ngx_http_geoip2_module > /dev/null 2>&1
 echo Build nginx.
 cd ..
 auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
---add-module=modules/ngx_brotli --add-module=modules/headers-more-nginx-module \
---add-module=modules/ngx_http_geoip2_module --conf-path=/etc/nginx/nginx.conf \
+--add-module=modules/ngx_brotli --add-module=modules/ngx_http_geoip2_module \
+--conf-path=/etc/nginx/nginx.conf \
 --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log \
 --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock \
 --http-client-body-temp-path=/var/cache/nginx/client_temp \
 --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
 --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
---user=nginx --group=nginx --without-select_module --without-poll_module \
---with-file-aio --with-threads --with-http_ssl_module --with-http_v2_module \
---with-http_v3_module --with-http_sub_module --without-http_access_module \
---without-http_autoindex_module --without-http_browser_module \
---without-http_charset_module --without-http_empty_gif_module \
---without-http_limit_conn_module --without-http_memcached_module \
---without-http_mirror_module --without-http_referer_module \
---without-http_split_clients_module --without-http_scgi_module \
---without-http_ssi_module --without-http_upstream_hash_module \
---without-http_upstream_ip_hash_module --without-http_upstream_keepalive_module \
---without-http_upstream_least_conn_module --without-http_upstream_random_module \
---without-http_upstream_zone_module --without-http_uwsgi_module \
+--user=nginx --group=nginx \
+--with-file-aio --with-threads --with-http_sub_module \
+--with-http_ssl_module --with-http_v2_module --with-http_v3_module \
+--without-select_module --without-poll_module \
+--without-http_access_module --without-http_autoindex_module \
+--without-http_browser_module --without-http_charset_module \
+--without-http_empty_gif_module --without-http_limit_conn_module \
+--without-http_memcached_module --without-http_mirror_module \
+--without-http_referer_module --without-http_split_clients_module \
+--without-http_scgi_module --without-http_ssi_module \
+--without-http_upstream_hash_module --without-http_upstream_ip_hash_module \
+--without-http_upstream_keepalive_module --without-http_upstream_least_conn_module \
+--without-http_upstream_random_module --without-http_upstream_zone_module \
+--without-http_uwsgi_module \
 --with-pcre=modules/pcre2 --with-pcre-opt=-ljemalloc --with-pcre-jit \
---with-zlib=modules/zlib --with-zlib-opt=-ljemalloc --with-openssl=modules/openssl \
+--with-zlib=modules/zlib --with-zlib-opt=-ljemalloc \
+--with-openssl=modules/openssl \
 --with-openssl-opt="-ljemalloc enable-ec_nistp_64_gcc_128 enable-weak-ssl-ciphers" \
 --with-ld-opt=-ljemalloc > /dev/null 2>&1
 make -j$(nproc) > /dev/null 2>&1
