ec66201999e739f204ce7848ca026e290b2a64b5
xuechen <me@ononoki.org>
2023-01-26 09:50:36 +0000

    Add image_filter module (#20)
    * Update build.sh
    
    * Update README.md
    
    * Update build.sh
    
    * Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo Install dependencies.
 apt-get update > /dev/null 2>&1
 apt-get install --allow-change-held-packages --allow-downgrades --allow-remove-essential \
 -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -fy \
-cmake curl git libmaxminddb-dev libpcre2-dev mercurial zlib1g-dev > /dev/null 2>&1
+cmake git libgd-dev libmaxminddb-dev libpcre2-dev mercurial zlib1g-dev > /dev/null 2>&1
 echo Fetch nginx-quic source code.
 hg clone -b quic https://hg.nginx.org/nginx-quic > /dev/null 2>&1
 echo Fetch quictls source code.
@@ -31,7 +31,7 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --user=www-data --group=www-data \
 --with-file-aio --with-threads --with-pcre-jit --with-http_sub_module \
 --with-http_ssl_module --with-http_v2_module --with-http_v3_module \
---without-select_module --without-poll_module \
+--with-http_image_filter_module --without-select_module --without-poll_module \
 --without-http_access_module --without-http_autoindex_module \
 --without-http_browser_module --without-http_charset_module \
 --without-http_empty_gif_module --without-http_limit_conn_module \
