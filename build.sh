ec8ac5dc13e0ed93feb1cf82a9ed3bf8eeb2a69c
ononoki <me@ononoki.org>
2025-12-03 17:52:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -45,8 +45,8 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --without-http_upstream_hash_module --without-http_upstream_ip_hash_module \
 --without-http_upstream_keepalive_module --without-http_upstream_least_conn_module \
 --without-http_upstream_random_module --without-http_upstream_zone_module \
---with-openssl=modules/quictls > /dev/null 2>&1
-make -j$(nproc) > /dev/null 2>&1
+--with-openssl=modules/quictls
+make -j$(nproc)
 cp objs/nginx ..
 cd ..
 hash=$(ls -l nginx | awk '{print $5}')
